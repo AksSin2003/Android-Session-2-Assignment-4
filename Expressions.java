7e9a361d744b138f7349dd90f58d024b3cb5c9cc
AksSin2003 <akshitsinha@ymail.com>
2016-06-14 15:56:37 +0000

    Algebraic Expressions
    Assignment 4 of session 2 in Android

---
+++ Expressions.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author Akshit
+ *
+ */
+public class Expressions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5, y = 10 ;
+		int exp1 = x + y * 2 ;
+		int exp2 = x - y + 2 ;
+		int exp3 = (x+y) * 2 ;
+		int exp4 = y%x ;
+		System.out.println(exp1);
+		System.out.println(exp2);
+		System.out.println(exp3);
+		System.out.println(exp4);
+	
+
+	}
+
+}
